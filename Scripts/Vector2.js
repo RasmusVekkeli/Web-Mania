fc12be6f6a27aa7b78b2d3fb20f3f4ecaefcf661
Rasmus Vekkeli <rasmusj.vekkeli@outlook.com>
2018-10-24 14:51:55 +0300

    Implemented base Vector2 class

---
+++ Scripts/Vector2.js
@@ -0,0 +1,22 @@
+/*
+ * Vector2 class contains coordinates to a point
+ * 
+ * x: X-coordinate of a point
+ * y: Y-coordinate of a point
+ * 
+ * Constructor parameters:
+ * x: Desired X-coordinate of the point, if not set, the value will be set as null
+ * y: Desired Y-coordinate of the point, if not set, the value will be set as null
+ * 
+ * Constructor will throw an exception if the parameter types are not number
+*/
+class Vector2 {
+	constructor(x, y) {
+		if (typeof x != "number" || typeof y != "number") {
+			throw new TypeError("Unexpected parameter types! Expected number, number; received " + typeof x + ", " + typeof y + ".");
+		}
+
+		this.x = (x === undefined ? null : x); 
+		this.y = (y === undefined ? null : y); 
+	}
+}
