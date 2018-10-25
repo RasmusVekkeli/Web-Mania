7fd956a4d23f664face698b0ee2f7c48c7252ffc
Rasmus Vekkeli <rasmusj.vekkeli@outlook.com>
2018-10-25 16:52:21 +0300

    Implemented new TypeOf function in GenerateTypeErrorString function

--- Scripts/GenerateTypeErrorString.js
+++ Scripts/GenerateTypeErrorString.js
@@ -3,22 +3,30 @@
  * 
  * Function parameters:
  * parameters: array of parameters of the function that failed its type check
- * expectedParameters: array of expected parameters of the function that failed its type check
+ * expectedParameterTypes: array of expected parameter types of the function that failed its type check. See the list of acceptable strings in TypeOf.js
  * 
  * Return value: String
  * Returns a string which contains a list of received and expected parameters types
 */
-function GenerateTypeErrorString(parameters, expectedParameters) {
+function GenerateTypeErrorString(parameters, expectedParameterTypes) {
 	var errorString = "Expected";
 
-	for (let i = 0; i < expectedParameters.length; i++) {
-		errorString += " " + expectedParameters[i];
+	for (let i = 0; i < expectedParameterTypes.length; i++) {
+		if (i != 0) {
+			errorString += ",";
+		}
+
+		errorString += " <" + expectedParameterTypes[i] + ">";
 	}
 
 	errorString += "; received";
 
 	for (let i = 0; i < parameters.length; i++) {
-		errorString += " " + typeof parameters[i];
+		if (i != 0) {
+			errorString += ",";
+		}
+
+		errorString += " <" + TypeOf(parameters[i]) + ">";
 	}
 
 	errorString += ".";
