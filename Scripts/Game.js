5126ca815902b59ea8e84e5b2da5994a4bb25fcb
Rasmus Vekkeli <camopoltergeist@outlook.com>
2018-10-30 13:53:20 +0200

    Set canvas width and height in constructor.

--- Scripts/Game.js
+++ Scripts/Game.js
@@ -9,5 +9,15 @@
 class Game {
 	constructor() {
 		this.context = document.getElementById("game_canvas").getContext("2d");
+		this.context.canvas.width = 1920;
+		this.context.canvas.height = 1080;
+	}
+
+	get aspectRatio() {
+		return this.context.canvas.width / this.context.canvas.height;
+	}
+
+	get inverseAspectRatio() {
+		return this.context.canvas.height / this.context.canvas.width;
 	}
 }
