3fd06c36f76c80c292010bb0938def396b127ff7
Rasmus Vekkeli <camopoltergeist@outlook.com>
2018-10-30 13:52:17 +0200

    Added event listener for fullscreen changes.

--- Init.js
+++ Init.js
@@ -1,7 +1,8 @@
 //Add code and function calls here that need to be executed on start.
 
-//Add event listener for browser window resizes and resize the canvas's container when it occurs
+//Add event listener for browser window resizes and fullscreen changes and resize the canvas's container when it occurs
 addEventListener("resize", ReSize);
+addEventListener("fullscreenchange", ReSize);
 
 //Create global game object
 var game = new Game();
