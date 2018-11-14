45c1a2ed766687a9a3e927f6eb6024f7055169a8
Rasmus Vekkeli <rasmusj.vekkeli@outlook.com>
2018-11-14 02:24:29 +0200

    Changed long note type from string to number

--- Scripts/Note.js
+++ Scripts/Note.js
@@ -3,10 +3,15 @@
  * 
  * Properties:
  * time: Time point in milliseconds from start of chart which marks the point of the note
- * type: Type of the note as string. Can be "normal", "longNoteStart" or "longNoteEnd"
+ * type: Type of the note as string. 
+ * 
+ * Type table:
+ * normal: 0
+ * long note start: 1
+ * longNoteEnd: 2
 */
 class Note {
-	constructor(time, type = "normal") {
+	constructor(time, type = 0) {
 		this.time = time;
 		this.type = type;
 	}
