a2d1e5702a6efa18c798c8a165235c19d551c0e9
Rasmus Vekkeli <rasmusj.vekkeli@outlook.com>
2018-12-26 02:02:18 +0200

    Fixed typos

--- Scripts/KeyConfig.js
+++ Scripts/KeyConfig.js
@@ -1,5 +1,5 @@
 class KeyConfig {
-	constructor(keys, laneWidth, hitPosition, downScroll, scrollSpeedMult, specialLane, specialLaneLeft, barNoteheight, noteSkin, snapColours, beatLineHeight, beatLineColour) {
+	constructor(keys, laneWidth, hitPosition, downScroll, scrollSpeedMult, specialLane, specialLaneLeft, barNoteHeight, noteSkin, snapColours, beatLineHeight, beatLineColour) {
 		//Uhh, I wonder if I shouldn't have made this constructor this convoluted
 		this.laneWidth = laneWidth;
 		this.hitPosition = hitPosition;
@@ -7,7 +7,7 @@ class KeyConfig {
 		this.scrollSpeedMult = scrollSpeedMult;
 		this.specialLane = specialLane;
 		this.specialLaneLeft = specialLaneLeft;
-		this.barNoteheight = barNoteheight;
+		this.barNoteHeight = barNoteHeight;
 		this.noteSkin = noteSkin;
 		this.snapColours = snapColours;
 		this.beatLineHeight = beatLineHeight;
