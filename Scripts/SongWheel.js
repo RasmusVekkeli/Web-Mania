c19ffdf3a2d1608b8dd4b5f6b1607ad433f37dd1
Rasmus Vekkeli <rasmusj.vekkeli@outlook.com>
2019-01-17 14:38:47 +0200

    Added menu for individual charts in chartlist of each song

--- Scripts/SongWheel.js
+++ Scripts/SongWheel.js
@@ -6,6 +6,8 @@ class SongWheel extends GameObject {
 		this.topIndex = 0;
 		this.selectionIndex = 0;
 		this.selectionPadding = 1;
+		this.songSelectedIndex = null;
+		this.diffSelectionIndex = 0;
 
 		this.mainTextSize = 34;
 
@@ -24,14 +26,6 @@ class SongWheel extends GameObject {
 		game.context.strokeStyle = "#FFFFFF";
 		game.context.strokeRect(this.x, this.pos.y, this.width, this.pos.h);
 
-		function getLoopingFromArray(array, index) {
-			if (index < 0) {
-				return array[((index % array.length) + array.length) % array.length]; //what
-			}
-
-			return array[index % array.length];
-		}
-
 		//Draw song panels
 		let firstY = this.firstSongPanelY;
 
@@ -41,27 +35,21 @@ class SongWheel extends GameObject {
 		game.context.fillStyle = "rgba(0, 0, 0, 0.6)";
 		game.context.fillRect(this.x, this.pos.y, this.width, this.pos.h);
 
+		let diffsDrawn = false;
+
 		for (let i = 0; i < this.songPanelCount; i++) {
-			//Selection indicator
-			if (i + this.topIndex == this.selectionIndex) {
-				game.context.fillStyle = "rgba(255, 255, 255, 0.3)";
-				game.context.fillRect(this.x, firstY + this.songPanelHeight * i, this.width, this.songPanelHeight);
+			if (i <= this.selectionPadding || this.songSelectedIndex === null) {
+				this.DrawSongPanel(i, firstY);
 			}
-
-			//Panel border
-			game.context.strokeStyle = "rgba(255, 255, 255, 1.0)";
-			game.context.strokeRect(this.x, firstY + this.songPanelHeight * i, this.width, this.songPanelHeight);
-
-			//Song name text
-			game.context.fillStyle = "rgba(255, 255, 255, 1.0)";
-			game.context.font = this.mainTextSize + "px Arial";
-			game.context.fillText(getLoopingFromArray(game.songList, i + this.topIndex).title, this.x + this.textPadding, firstY + this.songPanelHeight * i + this.textPadding);
-
-			//Artist text
-			game.context.font = this.mainTextSize / 2 + "px Arial";
-			game.context.fillText(getLoopingFromArray(game.songList, i + this.topIndex).artist, this.x + this.textPadding, this.mainTextSize + firstY + this.songPanelHeight * i + this.textPadding * 2);
-			//Chart author text
-			game.context.fillText(getLoopingFromArray(game.songList, i + this.topIndex).author, this.x + this.textPadding, this.mainTextSize * 1.5 + firstY + this.songPanelHeight * i + this.textPadding * 3);
+			else {
+				if (!diffsDrawn) {
+					for (let j = 0; j < game.songList.getLooping(this.songSelectedIndex).chartList.length; j++) {
+						this.DrawDiffPanel(j, firstY);
+					}
+					diffsDrawn = true;
+				}
+				this.DrawSongPanel(i, firstY, this.totalDiffPanelHeight);
+			}
 		}
 	}
 
@@ -75,6 +63,10 @@ class SongWheel extends GameObject {
 
 	get songPanelHeight() {
 		return this.textPadding * 4 + this.mainTextSize * 2;
+	}
+
+	get diffPanelHeight() {
+		return this.textPadding * 2 + this.mainTextSize;
 	}
 
 	get songPanelFitCount() {
@@ -94,6 +86,10 @@ class SongWheel extends GameObject {
 		return this.songPanelCount * this.songPanelHeight;
 	}
 
+	get totalDiffPanelHeight() {
+		return this.songSelectedIndex === null ? 0 : game.songList.getLooping(this.songSelectedIndex).chartList.length * this.diffPanelHeight;
+	}
+
 	get firstSongPanelY() {
 		return this.pos.y + this.pos.h / 2 - this.totalSongPanelHeight / 2;
 	}
@@ -105,10 +101,14 @@ class SongWheel extends GameObject {
 				return;
 			}
 
+			this.DeSelectSong();
+
 			this.selectionIndex += relIndex;
 		}
 		//If there's more than screen filling amount of songs
 		else {
+			this.DeSelectSong();
+
 			this.selectionIndex += relIndex;
 
 			if (this.selectionIndex < this.topIndex + this.selectionPadding) {
@@ -118,5 +118,57 @@ class SongWheel extends GameObject {
 				this.topIndex = this.selectionIndex + 1 - (this.songPanelCount - this.selectionPadding);
 			}
 		}
+	}
+
+	DrawSongPanel(i, firstY, offset = 0) {
+		//Selection indicator
+		if (i + this.topIndex == this.selectionIndex) {
+			game.context.fillStyle = "rgba(255, 255, 255, 0.3)";
+			game.context.fillRect(this.x, firstY + this.songPanelHeight * i + offset, this.width, this.songPanelHeight);
+		}
+
+		//Panel border
+		game.context.strokeStyle = "rgba(255, 255, 255, 1.0)";
+		game.context.strokeRect(this.x, firstY + this.songPanelHeight * i + offset, this.width, this.songPanelHeight);
+
+		//Song name text
+		game.context.fillStyle = "rgba(255, 255, 255, 1.0)";
+		game.context.font = this.mainTextSize + "px Arial";
+		game.context.fillText(game.songList.getLooping(i + this.topIndex).title, this.x + this.textPadding, firstY + this.songPanelHeight * i + this.textPadding + offset);
+
+		//Artist text
+		game.context.font = this.mainTextSize / 2 + "px Arial";
+		game.context.fillText(game.songList.getLooping(i + this.topIndex).artist, this.x + this.textPadding, this.mainTextSize + firstY + this.songPanelHeight * i + this.textPadding * 2 + offset);
+		//Chart author text
+		game.context.fillText(game.songList.getLooping(i + this.topIndex).author, this.x + this.textPadding, this.mainTextSize * 1.5 + firstY + this.songPanelHeight * i + this.textPadding * 3 + offset);
+	}
+
+	DrawDiffPanel(diffIndex, firstY) {
+		let offset = firstY + this.songPanelHeight + this.songPanelHeight * this.selectionPadding;
+		let diff = game.songList.getLooping(this.songSelectedIndex).chartList[diffIndex];
+
+		if (this.diffSelectionIndex == diffIndex) {
+			game.context.fillStyle = "rgba(255, 255, 255, 0.3)";
+			game.context.fillRect(this.x, offset + diffIndex * this.diffPanelHeight, this.width, this.diffPanelHeight);
+		}
+
+		let keyCountText = "[" + diff.keyCount + (diff.keyCount == 1 ? " key] " : " keys] ");
+		game.context.font = this.mainTextSize + "px Arial";
+
+		game.context.fillStyle = "#FFE500";
+		game.context.fillText(keyCountText, this.x + this.textPadding, offset + this.textPadding + diffIndex * this.diffPanelHeight);
+
+		game.context.fillStyle = "#FFFFFF";
+		game.context.fillText(diff.chartName, this.x + this.textPadding + game.context.measureText(keyCountText).width, offset + this.textPadding + diffIndex * this.diffPanelHeight);
+	}
+
+	SelectSong() {
+		this.songSelectedIndex = this.selectionIndex;
+		this.topIndex = this.songSelectedIndex - this.selectionPadding;
+		this.diffSelectionIndex = 0;
+	}
+
+	DeSelectSong() {
+		this.songSelectedIndex = null;
 	}
 }
