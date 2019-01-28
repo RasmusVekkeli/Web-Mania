2e230fcaa5f81e1b42411a26abd7d5f947832dd5
Rasmus Vekkeli <camopoltergeist@outlook.com>
2019-01-28 08:30:27 +0200

    Updated comments

--- Scripts/Combo.js
+++ Scripts/Combo.js
@@ -1,3 +1,43 @@
+/*
+ * Class which handles the combo UI element
+ * 
+ * Inherits from GameObject class
+ * 
+ * Properties:
+ * pos: A Rect object which is used for positioning the element on screen. Only pos.y can be changed with effect.
+ * comboNumber: An UIText object used for the combo number part of the combo element
+ * comboText: An UIText object used for the literal "COMBO" text under the combo number
+ * margin: A margin in pixels between the combo number and the "COMBO" text
+ * animationStart: A timestamp that marks start of last animation
+ * animationLenght: Total lenght of animation in milliseconds
+ * smallSize: Smallest size of the combo number, normal size (animation size)
+ * largeSize: Largest size of the combo number, peak animation size (animation size)
+ * maxSizeCombo: Determines at what combo the number will be the largest (combo multiplier size)
+ * smallMaxComboSizeAdd: Determines the amount added to smallSize value at maxSizeCombo
+ * largeMaxComboSizeAdd: Determines the amount added to largeSize value at maxSizeCombo
+ * time: Marks current time. Updates in Update-function.
+ * 
+ * Constructor properties: none
+ * 
+ * 
+ * Functions:
+ * Animate: Sets animationStart at current time resulting in animation in Draw function
+ * Parameters: none
+ * 
+ * 
+ * Update: Updates various variables each frame of the game
+ * Parameters: none
+ * 
+ * 
+ * Draw: Draws the element on screen
+ * Parameters: none
+ * 
+ * 
+ * get t: Returns a linear interpolation value between 0 and 1 for animation
+ * 
+ * 
+ * get size_t: Returns a linear interpolation value between 0 and 1 for combo size additions
+ */
 class Combo extends GameObject{
 	constructor() {
 		super(false, false, true);
@@ -82,7 +122,7 @@ class Combo extends GameObject{
 		return t;
 	}
 
-	get size_t() {
+	get size_t() { //c++ intesifies
 		let t = game.currentCombo / this.maxSizeCombo;
 
 		if (t < 0) {
