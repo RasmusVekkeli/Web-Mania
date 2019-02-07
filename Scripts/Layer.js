82f6941875f775bad420256c6c2c0263ab2ba7ef
Rasmus Vekkeli <rasmusj.vekkeli@outlook.com>
2019-02-07 11:54:05 +0200

    Updated comments

--- Scripts/Layer.js
+++ Scripts/Layer.js
@@ -1,3 +1,23 @@
+/*
+ * Class for grouping and managing updateable and rederable objects.
+ * 
+ * Inherits from GameObject class.
+ * 
+ * Properties: 
+ * name: Name as string for the layer. Can be any string. Used for searching for the layer with Game.GetLayerByName function.
+ * objectList: Array of updateable (has Update function implemented) and renderable (has Draw function implemented) objects.
+ * 
+ * Constructor parameters:
+ * name: String of desired name for the layer.
+ * objects: Array of objects. See objectList property above.
+ * 
+ * Functions:
+ * Update: Calls Update function on all objects in objectList array.
+ * Parameters: none
+ * 
+ * Draw: Calls Draw function on all objects in objectList array.
+ * Parameters: none
+*/
 class Layer extends GameObject {
 	constructor(name, objects) {
 		super(false, false, true);
