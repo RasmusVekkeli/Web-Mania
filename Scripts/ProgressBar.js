3ddeb5cfc018556d0ef313c5db5096414d159b64
Rasmus Vekkeli <camopoltergeist@outlook.com>
2019-02-12 09:06:25 +0200

    Added text which indicates progress to the progress bar

--- Scripts/ProgressBar.js
+++ Scripts/ProgressBar.js
@@ -5,15 +5,28 @@ class ProgressBar extends GameObject {
 		this.pos = rect;
 		this.color = color;
 		this.t = 0;
+		this.progressText = new UIText("", this.pos.x, this.pos.y - 10, -1, 1, 30, "Arial");
 	}
 
 	Update() {
+		if (this.skipUpdate || game.state !== 2) {
+			return;
+		}
+
 		this.t = game.currentProgress / game.endProgress;
+
+		this.progressText.text = "Files processed: " + game.currentProgress + " out of " + game.endProgress;
 	}
 
 	Draw() {
+		if (this.skipDraw || game.state !== 2) {
+			return;
+		}
+
 		game.context.fillStyle = this.color;
 
 		game.context.fillRect(this.pos.x, this.pos.y, this.pos.w * this.t, this.pos.h);
+
+		this.progressText.Draw();
 	}
 }
